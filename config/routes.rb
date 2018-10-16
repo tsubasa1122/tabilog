09ca0b170f2a1e34940a79a123cf1fc13a9878d6
tsubasa <tsubasan1122@gmail.com>
2018-10-16 21:50:59 +0900

    route configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get "/" => 'home#top'
-  get 'favorites/index'
-  get 'home/top'
-  get 'trips/new'
-  get 'trips/show'
-  get 'trips/index'
+  root 'home#top'
+  get '/about' => "home#about"
+
+  devise_for :users, controllers: {
+      sessions:      'users/sessions',
+      passwords:     'users/passwords',
+      registrations: 'users/registrations'
+  }
+  resources :favorites, only: [:create, :show, :destroy ]
+  resources :trips, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  resources :reviews, only: [:new, :index, :show, :create, :edit, :update, :destroy]
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
